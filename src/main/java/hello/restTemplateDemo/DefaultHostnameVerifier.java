f8950a52df3d63977488d1120870c09bbeedada5
YasinZhang <271155397@qq.com>
2019-05-08 21:24:16 +0800

    add restTemplate file

---
+++ src/main/java/hello/restTemplateDemo/DefaultHostnameVerifier.java
@@ -0,0 +1,13 @@
+package hello.restTemplateDemo;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+class DefaultHostnameVerifier implements HostnameVerifier {
+
+	@Override
+	public boolean verify(String hostname, SSLSession session) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+}
